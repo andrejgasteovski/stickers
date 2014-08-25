51ada54d967f2b3fe0c8df3c0862571c66e1f9bb
andrej <andrej.gasteovski@gmail.com>
2014-08-25 13:59:01 +0200

    blog

---
+++ Stickers/Stickers/Blog.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Blog.aspx.cs" Inherits="Stickers.Blog" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+     
+     <style type="text/css">
+        .panel
+        {
+            background: yellow;
+            width: 60%;
+            height: 700px;
+            margin-left: auto;
+            margin-right: auto;
+            }
+     
+     </style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="ph">
+    
+        <asp:Panel ID="panel" runat="server" CssClass="panel">
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Stickers/Stickers/Blog.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Stickers
+{
+    public partial class Blog : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox tb1 = new TextBox();
+            tb1.Text = "Hey Guys, do you want to meet somewhere";
+            tb1.Enabled = false;
+            tb1.TextMode = TextBoxMode.MultiLine;
+            panel.Controls.Add(tb1);
+
+            TextBox tb2 = new TextBox();
+            tb2.Text = "Sure why not.. let's meet at the park..";
+            tb2.Enabled = false;
+            tb2.TextMode = TextBoxMode.MultiLine;
+            panel.Controls.Add(tb2);
+        }
+    }
+}
